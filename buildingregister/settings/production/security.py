f0364785d426e350cad335af95da049930fed39d
Kaj Siebert <kaj@k-si.com>
2021-11-07 23:26:41 +0000

    Configure production security to HTTPS only and 1y session

--- buildingregister/settings/production/security.py
+++ buildingregister/settings/production/security.py
@@ -2,4 +2,9 @@ import os
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_AGE = 86400 * 365  # 365 days in seconds
+
 ALLOWED_HOSTS = []
