f918a181053af472706d1b37e12d6ca3ef77b08b
Kaj Siebert <kaj@k-si.com>
2021-11-11 08:22:35 +0100

    Add date check to sign-in submission

--- register/views/index.py
+++ register/views/index.py
@@ -4,6 +4,7 @@ from django import dispatch
 from django.contrib.auth.decorators import login_required
 from django.db import transaction
 from django.shortcuts import render
+from django.utils.timezone import now
 from django.views.decorators.cache import never_cache
 from register.models import SignInRecord, AuditRecord
 
@@ -18,6 +19,10 @@ def _handle_sign_in_out(request):
     signed_in = SignInRecord.objects.user(request.user).today().open()
     audit = AuditRecord.objects.create_from_request(request)
     action = request.POST.get('action')
+    date = request.POST.get('date')
+    today = now().date().isoformat()
+    if date != today:
+        return dict(error=f"Sorry, you attempted to {action} for the wrong date.")
 
     if action == "sign-in" and signed_in.count() == 0:
         SignInRecord.objects.create(user=request.user, sign_in=audit)
@@ -26,18 +31,23 @@ def _handle_sign_in_out(request):
         signed_in.sign_out(audit)
         user_signed_out.send_robust(sender=SignInRecord, request=request, user=request.user, audit=audit)
 
+    return {}
+
 
 @login_required
 @never_cache
 def index(request):
+    context = {}
     if request.method == 'POST':
-        _handle_sign_in_out(request)
+        context = _handle_sign_in_out(request)
 
     todays_records = SignInRecord.objects.user(request.user).today()
     signed_in_records = todays_records.open()
 
     return render(request, 'register/index.html', dict(
+        today=now().date().isoformat(),
         todays_records=todays_records,
         signed_in_records=signed_in_records,
         user=request.user,
+        **context,
     ))
